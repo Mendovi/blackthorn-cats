301aed0c6cfb7cca941b685ce24cbafa970c1741
Mendovi <96690983+Mendovi@users.noreply.github.com>
2022-01-09 11:08:33 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onclick = function(e) {    
+    with(document.getElementById('pic').style)
+    left = e.pageX + 'px', top = e.pageY + 'px';
+} // это выше функция чтобы картинка двигалась по клику
+
+
